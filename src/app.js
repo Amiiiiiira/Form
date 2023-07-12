eaf2e84cecd564e73f67887c0ab675870da024d6
amira sani <amira4eva@googlemail.com>
2023-07-12 22:17:52 +0100

    login page revised - CSS added JS to button

---
+++ src/app.js
@@ -0,0 +1,7 @@
+function logCredentials() {
+    let username = document.getElementById('inputUsername').value;
+    let password = document.getElementById('inputPassword').value;
+    
+    console.log('inputUsername:', username);
+    console.log('inputPassword:', password);
+  }
