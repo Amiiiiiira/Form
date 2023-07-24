0b49a45c393d12055eda313b19775cc9944fbcce
amira sani <amira4eva@googlemail.com>
2023-07-24 21:53:44 +0100

    added the jquery to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-function logCredentials() {
-    let username = document.getElementById('inputUsername').value;
-    let password = document.getElementById('inputPassword').value;
-    
-    console.log('inputUsername:', username);
-    console.log('inputPassword:', password);
-  }
+function logCredentials(event) {
+  event.preventDefault()
+  console.log($('#inputUsername').val());
+  console.log($('#inputPassword').val());
+}
+  $('#button').on('click', logCredentials);
