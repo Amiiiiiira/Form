87f37ec2bad63e16e31b076d28a938d51e46db4d
amira sani <amira4eva@googlemail.com>
2023-07-25 21:30:59 +0100

    ADDED API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
-function logCredentials(event) {
-  event.preventDefault()
-  console.log($('#inputUsername').val());
-  console.log($('#inputPassword').val());
+// function logCredentials(event) {
+//   event.preventDefault()
+//   console.log($('#inputUsername').val());
+//   console.log($('#inputPassword').val());
+// }
+//   $('#button').on('click', logCredentials);
+
+
+function fetchData(event){
+event.preventDefault()
+fetch('https://dummyjson.com/products/categories')
+.then(res => res.json())
+.then(data => console.log(data));
 }
-  $('#button').on('click', logCredentials);
+
+$('#button').on('click', fetchData);
